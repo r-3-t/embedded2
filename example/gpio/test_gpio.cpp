3b151929efc50484a4d1e91c3e5814e53b340858
jp <jp@r-3-t.org>
2015-05-21 19:52:37 +0200

    update test_gpio example with the new refactoring of the gpio class

--- example/gpio/test_gpio.cpp
+++ example/gpio/test_gpio.cpp
@@ -2,22 +2,26 @@
 
 #include <mcu_internal.h>
 
+#if defined STM32F407
 
 int main()
 {
-	gpio::gpio gpio1(PD12, gpio::NoPull, gpio::PushPull, gpio::Output);
-
+	gpio::gpio gpio1(PD12);
+	gpio1.set_as_digital_output(gpio::PushPull);
 	gpio1.on();
 
-	gpio::gpio gpio2(PD13, gpio::NoPull, gpio::PushPull, gpio::Output);
+	gpio::gpio gpio2(PD13);
+	gpio2.set_as_digital_output(gpio::PushPull);
 	gpio2.on();
 
 
-	gpio::gpio gpio3(PD14, gpio::NoPull, gpio::PushPull, gpio::Output);
+	gpio::gpio gpio3(PD14);
+	gpio3.set_as_digital_output(gpio::PushPull);
 	gpio3.on();
 
 
-	gpio::gpio gpio4(PD15, gpio::NoPull, gpio::PushPull, gpio::Output);
+	gpio::gpio gpio4(PD15);
+	gpio4.set_as_digital_output(gpio::PushPull);
 	gpio4.on();
 
 
@@ -26,4 +30,23 @@ int main()
 	return 0;
 }
 
+#elif defined STM32F103
+
+int main()
+{
+	gpio::gpio gpio1(PC13);
+	gpio1.set_as_digital_output(gpio::PushPull);
+	gpio1.on();
+
+
+	while(1) {};
+
+	return 0;
+}
+
+#else
+
+#error "No known mcu for this example"
+
+#endif
 
